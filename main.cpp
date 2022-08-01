69541b63a06bdfbb1d3eb0995e0abf472340b7a3
Ivan Plotnikov <plotnikov@protonmail.com>
2022-08-01 23:09:05 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "allocator.h"
+
+int main(int, char **)
+{
+    std::cout << "Homework 03: allocator" << std::endl;
+    std::cout << "===================================\n";
+
+
+
+	return 0;
+}
---
+++ tests_allocator.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE tests_ip
+
+
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(tests_ip)
+/*
+BOOST_AUTO_TEST_CASE(test_valid_version) {
+	BOOST_CHECK(version() > 0);
+}
+*/
+BOOST_AUTO_TEST_SUITE_END()
+
