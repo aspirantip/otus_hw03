0ba2ae2f4410497fe501f9f456bd33249096ef0c
Ivan Plotnikov <plotnikov@protonmail.com>
2022-08-03 00:38:08 +0300

    deleted tests

--- tests_allocator.cpp
+++ tests_allocator.cpp
@@ -1,13 +1,13 @@
-#define BOOST_TEST_MODULE tests_ip
+#define BOOST_TEST_MODULE tests_allocator
 
 
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(tests_ip)
+BOOST_AUTO_TEST_SUITE(tests_allocator)
 /*
 BOOST_AUTO_TEST_CASE(test_valid_version) {
-	BOOST_CHECK(version() > 0);
+    BOOST_CHECK(version() > 0);
 }
 */
 BOOST_AUTO_TEST_SUITE_END()
